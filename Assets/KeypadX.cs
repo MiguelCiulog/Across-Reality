fc4d69bf9b6b8c37602746a4c723b4c0034eca60
octaviot16 <octavio.torres216764@potros.itson.edu.mx>
2022-12-06 17:45:08 -0700

    keypad
    El minijuego del kepad

---
+++ Assets/KeypadX.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeypadX : MonoBehaviour
+{
+    public TMP_Text numeroX;
+    public TMP_Text numeroY;
+    public TMP_Text mensaje;
+    public TMP_Text numeroReto;
+    private void Start(){
+        numeroX.text="X";
+        numeroReto.text="";
+        
+    }
+
+    public void ButtonClicked(string number){
+        numeroX.text = number;
+        verificarCompletado();
+        
+    }
+
+    public void verificarCompletado(){
+        if (numeroX.text != "" && numeroY.text != ""){
+            if (numeroY.text != "Y"){
+                string numReto = numeroReto.text;
+                string texto1 = numeroX.text;
+                string texto2 = numeroY.text;
+                
+                int numero = int.Parse(numReto);
+                int num1 = int.Parse(texto1);
+                int num2 = int.Parse(texto2);
+
+                if (numero == (num1+num2)){
+                    mensaje.text = "¡COMPLETADO!";
+                }
+            }
+            
+        }
+    }
+}
